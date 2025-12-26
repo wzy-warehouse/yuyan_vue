ce5fc758a62afa41fe3fd566f12c24825d69759f
Qiuhuawa <1010565096@qq.com>
2025-12-26 08:30:07 +0800

    机制动态

--- src/components/dynamics.vue
+++ src/components/dynamics.vue
@@ -7,6 +7,25 @@
         <img src="@/assets/images/titleRight.png" alt="title right" />
       </div>
     </div>
+    <div class="dynamics-list">
+      <ul>
+        <li class="dynamics-item">
+          <router-link to="/dynamic1">20250109-"重大灾害无人机应急合作机制"业务交流和技术研讨会在哈尔滨召开</router-link>
+        </li>
+        <li class="dynamics-item">
+          <router-link to="/dynamic2">20250307-机制动态："重大灾害无人机应急合作机制"2025年考核和增补工作圆满完成</router-link>
+        </li>
+        <li class="dynamics-item">
+          <router-link to="/dynamic3">20250326-机制动态：聚青年英才之力 筑无人机应急防线</router-link>
+        </li>
+        <li class="dynamics-item">
+          <router-link to="/dynamic4">20250426-机制动态："雨燕应急"2025首期线上培训圆满完成，684名无人机应急尖兵云端集结</router-link>
+        </li>
+        <li class="dynamics-item">
+          <router-link to="/dynamic5">20250530-业务交流：国家减灾中心派员参加2025第九届世界无人机大会暨UASE无人机展</router-link>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -42,6 +61,35 @@
 }
 
 .title-blue {
+  color: #3F93DA;
+}
+
+.dynamics-list {
+  max-width: 1200px;
+  padding: 20px;
+}
+
+.dynamics-item {
+  padding: 10px 0;
+  border-bottom: 1px solid #eee;
+  list-style: none;
+}
+
+.dynamics-item:last-child {
+  border-bottom: none;
+}
+
+.dynamics-item a {
+  display: block;
+  padding: 10px;
+  text-decoration: none;
+  color: #333;
+  transition: background-color 0.3s;
+  border-radius: 4px;
+}
+
+.dynamics-item a:hover {
+  background-color: #f5f5f5;
   color: #3F93DA;
 }
 </style>
---
+++ src/router/index.js
@@ -0,0 +1,44 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+// 导入组件
+import App from '@/App.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: App
+  },
+  {
+    path: '/dynamic1',
+    name: 'Dynamic1',
+    component: () => import('@/views/dynamic/dynamic1.vue')
+  },
+  {
+    path: '/dynamic2',
+    name: 'Dynamic2',
+    component: () => import('@/views/dynamic/dynamic2.vue')
+  },
+  {
+    path: '/dynamic3',
+    name: 'Dynamic3',
+    component: () => import('@/views/dynamic/dynamic3.vue')
+  },
+  {
+    path: '/dynamic4',
+    name: 'Dynamic4',
+    component: () => import('@/views/dynamic/dynamic4.vue')
+  },
+  {
+    path: '/dynamic5',
+    name: 'Dynamic5',
+    component: () => import('@/views/dynamic/dynamic5.vue')
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+export default router
